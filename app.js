1af023065e15224a71e1d5cbb4d02b4ba9c594dc
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-13 21:19:28 +0000

    delete li by looking at UL (parent) first

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-var btns = document.querySelectorAll('#book-list .delete');
+const list = document.querySelector('#book-list ul');
 
-// turn to array
-Array.from(btns).forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-
-        // when hit delete button make it delete the li (parent) that the button is in
-        const li = e.target.parentElement;
-        li.parentNode.removeChild(li)
-
-
-    });
+// delete books through parent UL than applying it for each item which will take more resources
+list.addEventListener('click', function(e){
+   if(e.target.className == 'delete'){
+       const li = e.target.parentElement;
+       list.removeChild(li)
+   } 
 });
-
-// Prevent from navigating to site after clicking link
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('navigation to ', e.target.textContent, ' was prevented');
-})
