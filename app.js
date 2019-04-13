5b4e31a5f0dbdbcadec8c31519e474c549dc1215
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-13 20:05:18 +0000

    Lesson07 Parent Nodes and Children Nodes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const banner = document.querySelector('#page-banner');
+const bookList = document.querySelector('#book-list');
+// Show the parent elements of a element
+console.log('the parent node is:', bookList.parentNode);
+console.log('the parent element is:', bookList.parentElement.parentElement);
 
-console.log('#page-banner node type is:', banner.nodeType);
-console.log('#page-banner node name is:', banner.nodeName);
-console.log('#page-banner has child nodes is:', banner.hasChildNodes());
-
-// Clone a node with all nested content too
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+console.log(bookList.children);
