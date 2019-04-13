a5cb154f32d101618c23d313190b55ef42df43aa
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-13 19:56:13 +0000

    Completed Lesson06 with Nodes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var books = document.querySelectorAll('#book-list li .name');
+const banner = document.querySelector('#page-banner');
 
-Array.from(books).forEach(function(book){
-    book.textContent += ' (book title)';
-});
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes is:', banner.hasChildNodes());
 
-const bookList = document.querySelector('#book-list');
-// bookList.innerHTML = '<h2>Books and more books...</h2>';
-bookList.innerHTML += '<p>This is how you add HTML</p>';
+// Clone a node with all nested content too
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
