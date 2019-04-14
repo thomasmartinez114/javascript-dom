a078a19c19106d1981584f0d64fd7997b337856a
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-14 20:46:05 +0000

    Tab through content after clicking

--- app.js
+++ app.js
@@ -71,3 +71,20 @@ searchBar.addEventListener('keyup', function(e){
         }
     })
 });
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function(e){
+    if(e.target.tagName == 'LI'){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        // cycle through and check if it equals to targetPanel
+        panels.forEach(function(panel){
+            if (panel == targetPanel){
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active');
+            }
+        })
+    }
+})
