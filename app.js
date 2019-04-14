a64ce151650e65c49939fe9fa6f1743e4515997d
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-14 19:44:08 +0000

    Setting/Removing/Retrieving Attributes

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 const list = document.querySelector('#book-list ul');
 
 // delete books through parent UL than applying it for each item which will take more resources
-list.addEventListener('click', function(e){
-   if(e.target.className == 'delete'){
-       const li = e.target.parentElement;
-       list.removeChild(li)
-   } 
+list.addEventListener('click', function(e) {
+    if (e.target.className == 'delete') {
+        const li = e.target.parentElement;
+        list.removeChild(li)
+    }
 });
 
 // add book-list
-    // listen for submit & prevent it from refreshing page when hitting submit
+// listen for submit & prevent it from refreshing page when hitting submit
 const addForm = document.forms['add-book'];
 
-addForm.addEventListener('submit', function(e){
-   e.preventDefault();
-   const value = addForm.querySelector('input[type="text"]').value;
-   
+addForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+
     // create elements
     const li = document.createElement('li');
     const bookName = document.createElement('span');
     const deleteBtn = document.createElement('span');
-    
+
     // add content
     deleteBtn.textContent = 'delete';
     bookName.textContent = value;
-        
+
     // add/remove classes
     bookName.classList.add('name');
     deleteBtn.classList.add('delete');
-    
+
     // append to document
     li.appendChild(bookName);
     li.appendChild(deleteBtn);
     list.appendChild(li);
-    
-    
 });
+
+//      Attributes
+// 	• var.getAttribute('class')
+// 	• Var.setAttribute('class', 'name-2')
+// 	• Var.hasAttribute('class')
+// 	• Var.removeAttribute('class')
+
